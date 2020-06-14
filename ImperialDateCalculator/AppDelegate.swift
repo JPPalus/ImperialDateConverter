4b10370d75431074e1bf67730a61517a37e3a477
JP Palus <mal@malekith.host>
2020-06-14 17:43:36 +0200

    Initial Commit

---
+++ ImperialDateCalculator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ImperialDateCalculator
+//
+//  Created by Малекитъ on 14/06/2020.
+//  Copyright © 2020 Imperium of Man. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ImperialDateCalculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImperialDateCalculator
+//
+//  Created by Малекитъ on 14/06/2020.
+//  Copyright © 2020 Imperium of Man. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
